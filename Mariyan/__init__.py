c88152c7a438e8b083b5ef3195150928398caf54
Aditybot <riyazmariyan@gmail.com>
2024-01-14 17:35:20 +0530

    first commit

---
+++ Mariyan/__init__.py
@@ -0,0 +1,41 @@
+from Mariyan.core.bot import MariyanBot
+from Mariyan.core.dir import dirr
+from Mariyan.core.git import git
+from Mariyan.core.userbot import Userbot
+from Mariyan.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = MariyanBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
