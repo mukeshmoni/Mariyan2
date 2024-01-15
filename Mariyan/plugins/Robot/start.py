8b942256b6ea0850ced89c3fe3409273dcd1e19c
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-01-15 21:31:42 +0530

    Update start.py

--- Mariyan/plugins/Robot/start.py
+++ Mariyan/plugins/Robot/start.py
@@ -39,7 +39,6 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEJSahkho9v-jCh9JzrHs04jIjBEBoPMQAClAwAAgf7MFQfuxirE9ViPC8E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -188,8 +187,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAEJSahkho9v-jCh9JzrHs04jIjBEBoPMQAClAwAAgf7MFQfuxirE9ViPC8E")
-                await message.reply_photo(
+            await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
                         config.MUSIC_BOT_NAME
